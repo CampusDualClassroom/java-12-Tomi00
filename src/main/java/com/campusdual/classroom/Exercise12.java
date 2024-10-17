9fc679c61527e1fcba1b329432e724c66f1611de
Tomás Mañas Fernández <tomas.manas.fernandez@gmail.com>
2024-10-17 13:53:44 +0200

    Ejercicio 12 finalizado

--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -2,7 +2,42 @@ package com.campusdual.classroom;
 
 public class Exercise12 {
     public static void main(String[] args) {
+        Car myCar = new Car("Citroen", "Xsara", "Diesel");
 
+        if (myCar.isTachometerEqualToZero()){
+            System.out.println("El valor del tacómetro es: 0");
+        };
+
+        myCar.start();
+
+        myCar.start();
+
+        myCar.stop();
+
+        myCar.start();
+        myCar.accelerate();
+
+        myCar.accelerate();
+        myCar.accelerate();
+        myCar.accelerate();
+        myCar.accelerate();
+
+
+        myCar.brake();
+        myCar.brake();
+        myCar.brake();
+        myCar.brake();
+        myCar.brake();
+
+        myCar.turnAngleOfWheels(40);
+        myCar.turnAngleOfWheels(60);
+
+        myCar.setReverse(true);
+
+
+        myCar.accelerate();
+        myCar.setReverse(true);
+
+        }
     }
 
-}
